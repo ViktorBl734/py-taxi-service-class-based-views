bb1c9a0b462d9e54abb79e97f094490d84a8de3c
ViktorBl734 <harbourprogress@gmail.com>
2025-04-18 22:14:25 +0300

    Taxi service class-based views

--- taxi/urls.py
+++ taxi/urls.py
@@ -6,12 +6,12 @@ from .views import (index, CarListView, CarDetailView,
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturer", ManufacturerListView.as_view(),
+    path("manufacturer/", ManufacturerListView.as_view(),
          name="manufacturer-list"),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
-    path("driver/<int:pk>", DriverDetailView.as_view(), name="driver-detail"),
-    path("car/<int:pk>", CarDetailView.as_view(), name="car-detail")
+    path("driver/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"),
+    path("car/<int:pk>/", CarDetailView.as_view(), name="car-detail")
 ]
 
 app_name = "taxi"
