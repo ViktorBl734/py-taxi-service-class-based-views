2f9157672536a0f9bc73bd8f771596806a205726
ViktorBl734 <harbourprogress@gmail.com>
2025-04-18 22:03:00 +0300

    Taxi service class-based views

--- taxi/models.py
+++ taxi/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractUser
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
+    class Meta:
+        ordering = ['name']
 
 
 class Driver(AbstractUser):
--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from .views import index
+from .views import (index, CarListView, CarDetailView,
+                    DriverListView, DriverDetailView, ManufacturerListView)
+
 
 urlpatterns = [
     path("", index, name="index"),
+    path("manufacturer", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
+    path("driver/<int:pk>", DriverDetailView.as_view(), name="driver-detail"),
+    path("car/<int:pk>", CarDetailView.as_view(), name="car-detail")
 ]
 
 app_name = "taxi"
